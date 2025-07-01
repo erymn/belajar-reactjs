c14f6858ccd1010d91f7a8496b38dbec3e4e7cd8
DEVCOMP\erymn <erymnur@gmail.com>
2025-07-01 14:58:11 +0700

    Axios usage

--- create-react-app-tooling/react-basic-01/src/App.js
+++ create-react-app-tooling/react-basic-01/src/App.js
@@ -1,8 +1,13 @@
 import { useEffect, useState } from "react";
 import Tasks from "./components/Tasks";
+import Users from "./components/Users";
 
 function App() {
-  return <Tasks></Tasks>;
+  return (
+    <div>
+      <Users />
+    </div>
+  );
 }
 
 export default App;
---
+++ create-react-app-tooling/react-basic-01/src/components/Users.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+
+function Users() {
+  const getUsers = async () => {
+    //dengan keyword await
+    //const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+    const response = axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((data) => {
+        console.log(data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  // akan impact setiap kali browser di refresh
+  // karena axios asyncronous, maka fungsi pemanggilnya harus async
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return <div>Users</div>;
+}
+
+export default Users;
